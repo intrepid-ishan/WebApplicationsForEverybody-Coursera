82d1f3a59ad4f5512703dc0437b349b91028e41a
Ishan Makadia <45734338+intrepid-ishan@users.noreply.github.com>
2020-04-17 15:46:51 +0530

    Add files via upload

---
+++ Course3/code_assignment/Assignment-04/coursera_building_database_applications_in_php.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 17, 2020 at 12:12 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `coursera_building_database_applications_in_php`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `autos`
+--
+
+CREATE TABLE `autos` (
+  `auto_id` int(10) UNSIGNED NOT NULL,
+  `make` varchar(128) DEFAULT NULL,
+  `model` varchar(255) DEFAULT NULL,
+  `year` int(11) DEFAULT NULL,
+  `mileage` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `autos`
+--
+
+INSERT INTO `autos` (`auto_id`, `make`, `model`, `year`, `mileage`) VALUES
+(1, 'Rossers', 'Radialia', 39, 96),
+(2, 'Seguidilla', 'Toadyish', 80, 13),
+(4, 'fdsfsda', NULL, 8658, 84);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `autos`
+--
+ALTER TABLE `autos`
+  ADD PRIMARY KEY (`auto_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `autos`
+--
+ALTER TABLE `autos`
+  MODIFY `auto_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
